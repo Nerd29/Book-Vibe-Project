7c7e2c4ffb86ac9d0b0d2fb411d7fa675f61f01e
Nerd29 <majumderturja59@gmail.com>
2026-05-27 14:34:55 -0700

    done

--- src/main.jsx
+++ src/main.jsx
@@ -6,19 +6,16 @@ import { router } from './Routes/Routes'
 import { RouterProvider } from 'react-router/dom'
 import BookContext from './BookContext/BookContext'
 import { ToastContainer } from 'react-toastify'
-import { HashRouter } from 'react-router'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
   <BookContext>
 
   <RouterProvider router={router}/>
   <ToastContainer />
     </BookContext>
-    </HashRouter>
     
   </StrictMode>,
 )
